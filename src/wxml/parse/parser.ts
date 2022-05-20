377a9cf84a32b65021519d451b108911d5aff399
lujie <jie.lu3776@hipac.cn>
2022-05-20 21:01:11 +0800

    feat: 词法分析 OK

--- src/wxml/parse/parser.ts
+++ src/wxml/parse/parser.ts
@@ -2,10 +2,43 @@
  * @Author: 芦杰
  * @Date: 2022-05-20 16:37:38
  * @LastEditors: 芦杰
- * @LastEditTime: 2022-05-20 16:37:38
+ * @LastEditTime: 2022-05-20 20:57:01
  * @Description: 语法分析
  */
+import { Token, StackNode, Type } from './type'
 
-export default function parser(tokens) {
+interface State {
+  tokens: Token[]
+  cursor: number
+  stack: StackNode[]
+}
+
+export function parse(state: State) {
+  const { tokens, stack } = state
+  const nodes = stack[stack.length - 1].children
+  let { cursor } = state
+
+  while (cursor < tokens.length) {
+    const token = tokens[cursor]
+
+    if (token.type !== Type.TagStart) {
+      nodes.push(token)
+      cursor++
+      continue
+    }
+  }
+}
+
+export default function parser(tokens: Token[]) {
+  const state: State = {
+    tokens,
+    cursor: 0,
+    stack: [{
+      tagName: '',
+      children: []
+    }]
+  }
+
+  parse(state)
   return tokens
 }
--- src/wxml/parse/type.ts
+++ src/wxml/parse/type.ts
@@ -2,14 +2,24 @@
  * @Author: 芦杰
  * @Date: 2022-05-20 16:53:01
  * @LastEditors: 芦杰
- * @LastEditTime: 2022-05-20 17:27:43
+ * @LastEditTime: 2022-05-20 20:56:39
  * @Description: 类型
  */
 
 /** 节点类型 */
 export enum Type {
   /** 文本节点 */
-  Text = 'text'
+  Text = 'text',
+  /** 注释 */
+  Comment = 'comment',
+  /** tag 开始 */
+  TagStart = 'tag-start',
+  /** tag 结束 */
+  TagEnd = 'tag-end',
+  /** Tag 标签 */
+  Tag = 'tag',
+  /** 属性 */
+  Attribute = 'attribute'
 }
 
 /** 位置信息 */
@@ -22,16 +32,16 @@ export interface Position {
 /** 词法分析单元 */
 export interface Token {
   type: Type
-  content: string
-  position: {
-    start: Position
-    end: Position
+  content?: string
+  close?: boolean
+  position?: {
+    start?: Position
+    end?: Position
   }
 }
 
-/** 整个状态 */
-export interface State {
-  str: string
-  tokens: Token[]
-  position: Position
+/** 栈节点 */
+export interface StackNode {
+  tagName: string
+  children: Token[]
 }
